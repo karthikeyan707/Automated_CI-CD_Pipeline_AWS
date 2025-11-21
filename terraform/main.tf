08aee35adf59296880cfe8b15f69c126295d5ca7
KarthiKeyan <karthikeyanuk559@gmail.com>
2025-11-21 23:15:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,59 @@
 resource "aws_key_pair" "deployer" {
-   key_name = var.key_pair_name
-   public_key = file(var.public_key_path)
+  key_name   = var.key_pair_name
+  public_key = file(var.public_key_path)
 }
 
 resource "aws_security_group" "web_sg" {
-   name = "web-sg"
-   description = "Allow HTTP and SSH"
-   ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-   }
-   ingress {
-     from_port = 3000
-     to_port = 3000
-     protocol = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-     from_port = 0
-     to_port = 0
-     protocol = "-1"
-     cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 data "aws_ami" "ubuntu" {
-   owners = ["099720109477"]
-   most_recent = true
-   filter {
-      name = "name"
-      values = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
-   }
+  owners      = ["099720109477"] # Canonical
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
 }
 
 resource "aws_instance" "app" {
-   ami = data.aws_ami.ubuntu.id
-   instance_type = var.instance_type
-   key_name = aws_key_pair.deployer.key_name
-   vpc_security_group_ids = [aws_security_group.web_sg.id]
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-   user_data = file("${path.module}/ec2_userdata.sh")
+  user_data = file("${path.module}/ec2_userdata.sh")
 
-   tags {
-      Name = "cicd_demo_app"
-   }
+  tags = {
+    Name = "cicd_demo_app"
+  }
 }
 
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "your-bucket-name"
+  bucket = "your-unique-bucket-name"
 }
 
 resource "aws_s3_bucket_acl" "app_bucket_acl" {
