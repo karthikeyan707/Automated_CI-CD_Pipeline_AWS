0c0ab5f1042663ea989c7b6e7154f75f914b2eaa
KarthiKeyan <karthikeyanuk559@gmail.com>
2025-11-22 00:06:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "app" {
 }
 
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "karthikeyan_CICD_demo_bucket_aws"
+  bucket = "karthikeyan_cicd_demo_bucket_aws"
 }
 
 resource "aws_s3_bucket_acl" "app_bucket_acl" {
